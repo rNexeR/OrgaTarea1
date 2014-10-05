68fa5fc81b921431a238810fc49804df24f546e6
Turupawn <turupawn@userr>
2014-10-04 21:28:34 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// queue::push/pop
+#include "Mascota.h"
+#include "Evaluador.h"
+#include <iostream>
+#include <list>
+#include <fstream>
+using namespace std;
+
+//Escribe los datos del objeto mascota dado en un archivo binario con el nombre dado en la posicion dada
+//Nota: Se deben poder agregar varias mascotas en un solo archivo
+void escribir(string nombre_archivo, Mascota*mascota, int posicion)
+{
+}
+
+//Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
+//Nota: Se deben poder leer varias mascotas de un solo archivo
+Mascota* leer(string nombre_archivo, int posicion)
+{
+    Mascota *m = new Mascota(-1,"");
+    return m;
+}
+
+//Devuelve un vector que contenga todas las mascotas previamente escritas (con la funcion escribir()) en un archivo binario con nombre dado
+//Nota: El vector debe contener las mascotas ordenadas de acuerdo a la posicion en la que se escribieron
+vector<Mascota*> leerTodos(string nombre_archivo)
+{
+    vector<Mascota*>respuesta;
+    return respuesta;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
