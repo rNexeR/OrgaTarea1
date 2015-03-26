b513b75328398fae1001be3b9595c33493c24c3c
rnexer <rnexer@gmail.com>
2015-03-26 09:58:30 -0600

    s

--- main.cpp
+++ main.cpp
@@ -10,13 +10,28 @@ using namespace std;
 //Nota: Se deben poder agregar varias mascotas en un solo archivo
 void escribir(string nombre_archivo, Mascota*mascota, int posicion)
 {
+    nombre_archivo+=".txt";
+    ofstream out(nombre_archivo.c_str(), ios::app);
+    out.seekp(posicion*14,ios::beg);
+    out.write(mascota->nombre.c_str(), 10);
+    out.write((char*)&mascota->edad, 4);
+    out.close();
 }
 
 //Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
 //Nota: Se deben poder leer varias mascotas de un solo archivo
 Mascota* leer(string nombre_archivo, int posicion)
 {
-    Mascota *m = new Mascota(-1,"");
+    nombre_archivo+=".txt";
+    ifstream in(nombre_archivo.c_str());
+    in.seekg(posicion*14, ios::beg);
+    char* nombre = new char[10];
+    in.read(nombre, 10);
+    string n = nombre;
+    int edad;
+    in.read((char*)&edad, 4);
+    Mascota *m = new Mascota(edad, n);
+    in.close();
     return m;
 }
 
